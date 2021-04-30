b33062ccb74f448298fe3094141debeba70f195f
xFlappy <nic.degiorgi@gmail.com>
2021-04-30 16:08:20 +0200

    Last changes

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -56,11 +56,18 @@ class TransactionList extends StatelessWidget {
                   subtitle: Text(
                     DateFormat.yMMMd().format(transactions[index].date),
                   ),
-                  trailing: IconButton(
-                    icon: Icon(Icons.delete),
-                    color: Theme.of(context).errorColor,
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.height > 360
+                      ? FlatButton.icon(
+                          label: Text("Delete"),
+                          textColor: Theme.of(context).errorColor,
+                          icon: Icon(Icons.delete),
+                          onPressed: () => deleteTx(transactions[index].id),
+                        )
+                      : IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Theme.of(context).errorColor,
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
             },
